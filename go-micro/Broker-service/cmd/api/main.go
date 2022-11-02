206e2072e286be0a711cda374db8f033d1072792
Vladimir <vova_ocheret@icloud.com>
2022-11-02 22:45:57 +0000

    First commit for the microservices project

---
+++ go-micro/Broker-service/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker sservice on port %s/n", webPort)
+
+	//define htp server
+	srv := &http.Server{
+		Addr:    fmt.Sprint(":%s", webPort),
+		Handler: app.routes(),
+	}
+	//start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
